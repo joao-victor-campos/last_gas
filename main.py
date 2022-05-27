96981016c0dfff426765d84960b1fda14d455252
joao_victor_campos <mmoraes.victor@gmail.com>
2022-05-26 22:33:55 -0300

    bot working

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
-import discord 
-import random, datetime, asyncio, requests
- 
+
+
+from discord.ext import commands
+from datetime import datetime, time, timedelta
+import asyncio
 
 TOKEN = 'OTc5NTQ2MTI0NDcxODk4MTcy.GCfKmZ.qNjmi61tsHhKTqWEog2ro8dePaayxqX7dwGsyU'
+bot = commands.Bot(command_prefix="$")
+WHEN = time(1, 29, 0)  # 1:29 PM UTC
+channel_id = 338427143694581761 
 
-client = discord.Client()
-
-@client.event 
-async def on_ready():
-    print('último gás {0.user}'.format(client))
-
-
-# @client.command 
-async def schedule_daily_message():
-    now = datetime.datime.now()
-    # then = now + datetime.timedelta(days=1) 
-    then = now.replace(hour=22, minute=19)
-    wait_time = (then-now).total_sendods()
-    await asyncio.sleep(wait_time)
-
-    channel = client.get_chanell(338427143694581761)
-
+async def called_once_a_day():  
+    await bot.wait_until_ready() 
+    channel = bot.get_channel(channel_id) 
     await channel.send("https://www.youtube.com/watch?v=OZpx3loLxg8")
 
-client.run(TOKEN)
+async def background_task():
+    now = datetime.utcnow()
+    if now.time() > WHEN:  
+        tomorrow = datetime.combine(now.date() + timedelta(days=1), time(0))
+        seconds = (tomorrow - now).total_seconds()  
+        await asyncio.sleep(seconds)   # Sleep until tomorrow and then the loop will start 
+    while True:
+        now = datetime.utcnow() # You can do now() or a specific timezone if that matters, but I'll leave it with utcnow
+        target_time = datetime.combine(now.date(), WHEN)  
+        seconds_until_target = (target_time - now).total_seconds()
+        await asyncio.sleep(seconds_until_target)  # Sleep until we hit the target time
+        await called_once_a_day()  # Call the helper function that sends the message
+        tomorrow = datetime.combine(now.date() + timedelta(days=1), time(0))
+        seconds = (tomorrow - now).total_seconds()  # Seconds until tomorrow (midnight)
+        await asyncio.sleep(seconds)   # Sleep until tomorrow and then the loop will start a new iteration
+
+
+if __name__ == "__main__":
+    bot.loop.create_task(background_task())
+    bot.run(TOKEN)
