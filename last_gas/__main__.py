415cdbcc36c365a8e3c70a76f1a5d690257e6193
Victor Lúcio <victor.lucio10@gmail.com>
2022-07-03 20:11:32 -0300

    created promos command

--- last_gas/__main__.py
+++ last_gas/__main__.py
@@ -1,10 +1,72 @@
+import re
+from discord import Embed, Color
 from discord.ext import commands
 
 from last_gas.adapters import EnvVarConfigLoader
 from last_gas.domain.schedulers.time_scheduler import background_scheduler, SCHEDULES
+from last_gas.domain.sources import Pelando
 
 config_manager = EnvVarConfigLoader()
 bot = commands.Bot(command_prefix="$")
+
+
+@bot.command()
+async def promos(ctx, *search):
+    """
+    Returns a message with promos from Pelando website
+
+    usage: $promos search string [-limit of results]
+
+    :param ctx: context object
+    """    
+    if re.match(r"^\-[0-9]+", search[-1]):
+        limit = int(search[-1][1:])
+        search = search[:-1]
+    else:
+        limit = 5
+
+    search_str = " ".join(search)
+
+    source = Pelando()
+
+    # Check assets -> pelando_schema.graphql
+    columns = [
+        "title",
+        "price",
+        "temperature",
+        "discountFixed",
+        "discountPercentage",
+        "sourceUrl",
+        "image{url}",
+        "store{name}",
+        "couponCode",
+    ]
+
+    offers = source.search_offers(
+        search=search_str,
+        columns=columns,
+        limit=limit,
+    )
+
+    for offer in offers:
+        card = Embed(
+            title=offer["title"],
+            description=offer["store"]["name"],
+            url=offer["sourceUrl"],
+            color=Color.green(),
+        )
+
+        value_description = Pelando.get_value_description(offer)
+
+        card.set_thumbnail(url=offer["image"]["url"])
+        card.add_field(
+            name=value_description, value=f"❄️ {int(offer['temperature'])}° 🔥"
+        )
+
+        if offer["couponCode"]:
+            card.add_field(name="Cuponzin 🤑", value=offer["couponCode"])
+
+        await ctx.send(embed=card)
 
 
 if __name__ == "__main__":
@@ -20,4 +82,5 @@ if __name__ == "__main__":
                 **schedule["kwargs"],
             )
         )
+
     bot.run(configs["TOKEN"])
