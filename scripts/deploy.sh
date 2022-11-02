74e1e1f7d4a37fdec9d4a9a34b4915e5e2430ee3
Victor Poglioni <victorrenop16@gmail.com>
2022-11-02 19:09:39 -0300

    Change deploy script to get the image's latest version

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,5 @@ export $(grep -v '^#' creds.env | xargs -d '\n')
 docker stop last_gas 
 docker rm last_gas 
 docker rmi $(docker images -a -q)
-docker pull joaomoraes/last-gas
-docker run -d -e TOKEN --name last_gas joaomoraes/last-gas 
+docker pull joaomoraes/last-gas:latest
+docker run -d -e TOKEN --name last_gas joaomoraes/last-gas:latest
