9ca7955ae2b828958bda50d5f0716d66b8ee5306
João Victor Campos Alves Moraes <mmoraes.victor@gmail.com>
2022-11-02 16:24:52 -0300

    set TOKEN envvar

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,4 @@ export $(grep -v '^#' creds.env | xargs -d '\n')
 docker stop last_gas 
 docker rmi $(docker images -a -q)
 docker pull joaomoraes/last-gas
-docker run joaomoraes/last-gas -d -e TOKEN last_gas 
+docker run joaomoraes/last-gas -d -e TOKEN --name last_gas 
