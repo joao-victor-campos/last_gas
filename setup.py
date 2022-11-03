86335d7c69854a7797e7e32f1717d5bc0b1e82b5
Victor Renó Poglioni <victorrenop16@gmail.com>
2022-11-02 21:17:52 -0300

    Release 0.1.0 (#14)
    * Add release process on merging a release branch. Add setup file for versioning.
    
    * Change deploy script to get the image's latest version
    
    * Add missing new line to setup file

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+__version__ = "0.1.0"
+
+with open("README.md") as description_file:
+    readme = description_file.read()
+
+with open("requirements.txt") as requirements_file:
+    requirements = [line for line in requirements_file]
+
+setup(
+    name="last_gas",
+    version=__version__,
+    author="Menzolas",
+    python_requires=">=3.8.5",
+    description="Last Gas Discord Bot",
+    long_description=readme,
+    install_requires=requirements,
+    packages=find_packages(),
+)
