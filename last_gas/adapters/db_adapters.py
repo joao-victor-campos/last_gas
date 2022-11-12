369691ce481d33ad2d56ff659c449adefcbee35a
João Victor Campos Alves Moraes <mmoraes.victor@gmail.com>
2022-11-12 20:41:50 -0300

    update method

--- last_gas/adapters/db_adapters.py
+++ last_gas/adapters/db_adapters.py
@@ -3,7 +3,8 @@ from sqlalchemy import create_engine, Column, String, Integer, CHAR, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from typing import Any, Dict
-from datetime import datetime
+from datetime import datetime, timedelta
+from last_gas.domain.schedulers.time_scheduler import get_now_time
 import uuid
 
 Base = declarative_base()
@@ -12,13 +13,11 @@ Base = declarative_base()
 class Schedules(Base):
     tablename = "schedules"
 
-    schedule_id = Column("schedule_id", Integer, primary_key=True)
-    obj_name = Column("obj_name", String)
+    obj_name = Column("obj_name", String, primary_key=True)
     datetime_scheduled = Column("datetime_scheduled", DateTime)
     created_at = Column("create_at", DateTime)
 
-    def __init__(self, schedule_id, obj_name, datetime_scheduled, created_at) -> None:
-        self.schedule_id = schedule_id
+    def __init__(self, obj_name, datetime_scheduled, created_at) -> None:
         self.obj_name = obj_name
         self.datetime_scheduled = datetime_scheduled
         self.created_at = created_at
@@ -38,14 +37,9 @@ class PostgresLoader(DBLoader):
         obj: str,
         scheduled_time: str,
     ) -> None:
-        schedule = Schedules(
-            uuid.uuid4(),
-            obj,
-            scheduled_time,
-            datetime.now()
-        )
+        schedule = Schedules(uuid.uuid4(), obj, scheduled_time, get_now_time())
         self.session.add(schedule)
         self.session.commit()
 
-    def update():
-        pass
+    def update(self, obj: str, param, new_obj: str):
+        self.session.query(Schedules).filter(Schedules.obj_name == obj).update()
