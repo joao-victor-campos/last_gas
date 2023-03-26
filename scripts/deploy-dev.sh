8a1e137fac1704f4136fccb3a5f9bd346e2ef231
Victor Lucio <victor.lucio10@gmail.com>
2023-03-25 23:07:11 -0300

    removed restart for dev

--- scripts/deploy-dev.sh
+++ scripts/deploy-dev.sh
@@ -3,4 +3,4 @@ docker stop last_gas-dev
 docker rm last_gas-dev
 docker rmi $(docker images -a -q last_gas:dev)
 docker pull joaomoraes/last-gas:dev
-docker run --restart unless-stopped -d -e TOKEN=$TOKEN_DEV --name last_gas-dev joaomoraes/last-gas:dev
+docker run -d -e TOKEN=$TOKEN_DEV --name last_gas-dev joaomoraes/last-gas:dev
